225cb85e1e4bdda1f46d8c7e6ebf0483d322b45d
Foqz <fikskamil@gmail.com>
2018-11-19 21:32:48 +0100

    fixed controller

--- src/main/java/com/crud/library/controller/LibraryController.java
+++ src/main/java/com/crud/library/controller/LibraryController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("v1/library")
+@RequestMapping("/v1/library")
 public class LibraryController {
     @Autowired
     private DbService service;
